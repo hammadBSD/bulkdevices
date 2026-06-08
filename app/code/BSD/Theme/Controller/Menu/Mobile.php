e29aedcd41a7f847a0663588577329450af08452
Ubuntu <ubuntu@ip-172-26-13-242.ap-south-1.compute.internal>
2026-06-08 15:46:42 +0000

    homepage optimisation mobile completed

---
+++ app/code/BSD/Theme/Controller/Menu/Mobile.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BSD\Theme\Controller\Menu;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\Result\RawFactory;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\LayoutInterface;
+
+class Mobile implements HttpGetActionInterface
+{
+    public function __construct(
+        private readonly RawFactory $resultRawFactory,
+        private readonly LayoutInterface $layout
+    ) {
+    }
+
+    public function execute(): Raw
+    {
+        /** @var Template $block */
+        $block = $this->layout->createBlock(Template::class);
+        $block->setTemplate('Magento_Theme::html/header/menu/mobile-menu-panel.phtml');
+
+        return $this->resultRawFactory->create()
+            ->setHeader('Content-Type', 'text/html; charset=utf-8')
+            ->setContents($block->toHtml());
+    }
+}
